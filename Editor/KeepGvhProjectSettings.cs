65ec3c6c83518c9a6210ff2219eb9640bc615e89
baba-s <shotababa1206@gmail.com>
2022-12-24 16:04:05 +0900

    add

---
+++ Editor/KeepGvhProjectSettings.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.Compilation;
+
+namespace Kogane.Internal
+{
+    /// <summary>
+    /// GvhProjectSettings.xml が勝手に変更される不具合が GitHub の Issue で報告されているが
+    /// まだ対応されていないため、暫定で対応するエディタ拡張
+    /// https://github.com/googlesamples/unity-jar-resolver/issues/524
+    /// </summary>
+    [InitializeOnLoad]
+    internal static class KeepGvhProjectSettings
+    {
+        //================================================================================
+        // 関数(static)
+        //================================================================================
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        static KeepGvhProjectSettings()
+        {
+            CompilationPipeline.compilationStarted          += _ => Restore();
+            CompilationPipeline.compilationFinished         += _ => Restore();
+            CompilationPipeline.assemblyCompilationFinished += ( _, _ ) => Restore();
+            EditorApplication.delayCall                     += () => Restore();
+            EditorFocusWatcher.OnFocused                    += () => Restore();
+
+            Restore();
+
+            static void Restore()
+            {
+                var sourcePath = "ProjectSettings/GvhProjectSettings.xml.source";
+
+                if ( !File.Exists( sourcePath ) ) return;
+
+                File.Copy
+                (
+                    sourceFileName: sourcePath,
+                    destFileName: "ProjectSettings/GvhProjectSettings.xml",
+                    overwrite: true
+                );
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Kogane.KeepGvhProjectSettings
+# Kogane Keep Gvh Project Settings
+
+GvhProjectSettings.xml が勝手に変更される現象を防ぐエディタ拡張
+
+## 使用例
+
+ProjectSettings フォルダに「GvhProjectSettings.xml.source」というファイルを格納することで  
+コンパイル開始時や Unity エディタがフォーカスされた時に  
+「GvhProjectSettings.xml.source」を「GvhProjectSettings.xml」に上書きコピーします
